f86bb30f394be6954c1cdd91f5f9b1ac491cebc3
Erick <egromero@uc.cl>
2019-01-14 15:05:18 -0300

    first model scaffold for student

---
+++ AccessSystem/db/migrate/20190114130400_add_student_number_to_student.rb
@@ -0,0 +1,5 @@
+class AddStudentNumberToStudent < ActiveRecord::Migration[5.2]
+  def change
+    add_column :students, :student_number, :string
+  end
+end
--- AccessSystem/db/seeds.rb
+++ AccessSystem/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create(student_number: '14206374', name: 'Erick Romero', mail: 'egromero@uc.cl', ac_situation: 'Alumno Regular')
+Student.create(student_number: '13246544', name: 'Junito Perez', mail: 'jperez@uc.cl', ac_situation: 'Alumno Regular')
+Student.create(student_number: '12233454', name: 'Ramón Ayala', mail: 'dy@uc.cl', ac_situation: 'Egresado')
+Student.create(student_number: '18232374', name: 'Juan Carlos Ozuna', mail: 'jcozuna1@uc.cl', ac_situation: 'Suspendido')
